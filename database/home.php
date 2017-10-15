db4cfe2d501f79379fe397cc7dab6d1ebc4c0d17
tonny62 <ton_kriddanai@hotmail.com>
2017-10-15 16:08:03 +0700

    mycommit

--- database/home.php
+++ database/home.php
@@ -14,10 +14,9 @@
     <ul id="menu">
       <li><a href="room.php">Room</a></li>
       <li><a href="facility.php">Facility</a></li>
-      <li><a href="#">Special Offer</a></li>
-      <li><a href="#">Review</a></li>
+      <li><a href="review.php">Review</a></li>
+      <li><a href="login.php">Log In</a></li>
     </ul>
-
   </div>
   <div id="body">
     <table>
@@ -34,7 +33,49 @@
   </div>
 
 
+  <div id="box">
+    <!--booking-->
+    <form>
+      <div id="formcontainer">
+        &nbsp;&nbsp;&nbsp;&nbsp;Check availabity<br><br>
+        <div id="center">
+          Check in :&nbsp;&nbsp;<input type="date" placeholder="11/10/2560" /><br><br> Check out:&nbsp;&nbsp;<input type="date" placeholder="14/10/2560" /><br><br>
+        </div>
 
+        &nbsp;&nbsp;&nbsp;&nbsp;Property<br><br>
+        <div class="center">
+          Room:
+          <select>
+                <option>-</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>&nbsp;&nbsp;&nbsp; Adult:
+          <select>
+                <option>-</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>&nbsp;&nbsp;&nbsp; Children:
+          <select>
+                <option>-</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+        </div>
+        <br>
+        <div class="center">
+          <!--ปุ่ม-->
+          <input type="submit" value="Check Availability" />
+        </div>
+
+      </div>
+
+  </div>
 
 
 
