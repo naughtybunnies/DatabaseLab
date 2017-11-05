d694401d875e70c252adeec9626b00fb6c6a97a5
parewari <maroengsit.pwr@gmail.com>
2017-11-05 14:47:31 +0700

    room

--- database/room.php
+++ database/room.php
@@ -25,31 +25,52 @@
   </table>
   <div id="info">
     <h1>&nbsp;&nbsp;Hotel Room</h1>
+    <hr />
     <table>
       <tr>
         <td>
           <div class="center">
-            <div id="space"><img src="img/view1.jpg" height="200" width="300"></div>
+            <div id="space"><img src="img/s3.jpg" height="200" width="300"></div>
           </div>
         </td>
-        <td>Room1<br>something</td>
+        <td>
+          <h3>Deluxe Pool Access Room</h3><br>
+          <p>Room size: 53 Sqm. <br> Bedding: 1 King / 1 King 1 Single</p>
+          <p>Exquisite 2 clusters of 4 villas each set in a separate private area with direct access
+            to a cross shaped shared outrdoor pool.</p>
+            <hr>
+          </td>
       </tr>
       <tr>
         <td>
           <div class="center">
-            <div id="space"><img src="img/view1.jpg" height="200" width="300"></div>
+            <div id="space"><img src="img/s4.jpg" height="200" width="300"></div>
           </div>
         </td>
-        <td>Room2<br>something</td>
+        <td>
+          <h3>Ocean Pool Villa</h3><br>
+        <p>Room size: 53 Sqm. <br> Bedding: 1 King plus 1 Single /2 Queens</p>
+        <p>Set within the tropical garden compound with an outdoor
+          private pool (5M X 2M) nearby a private courtyard.
+          Separated bathroom comes with oversize bathtub and toilet. </p>
+          <hr>
+        </td>
       </tr>
       <tr>
         <td>
           <div class="center">
-            <div id="space"><img src="img/view1.jpg" height="200" width="300"></div>
+            <div id="space"><img src="img/s5.jpg" height="200" width="300"></div>
           </div>
         </td>
-        <td>Room3<br>something</td>
+        <td><h3>Garden Pool Villa</h3><br>
+        <p>Room size: 65 Sqm. <br> Bedding: 1 King/1 King plus 1 Single</p>
+        <p>Located beach front area for spectacular ocean view equipped
+          with gorgeous out-door private pool (6M X 2.3M).  Separated bathroom
+          comes with oversize bathtub, standing shower and toilet.</p>
+          <hr>
+        </td>
       </tr>
+
     </table>
   </div>
   <table id="bar">
