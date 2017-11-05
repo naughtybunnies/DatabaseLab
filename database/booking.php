c47f5d23495a7a14ad8674b8ad06f28ce95b88fd
tonny62 <ton_kriddanai@hotmail.com>
2017-11-05 11:58:56 +0700

    merge

--- database/booking.php
+++ database/booking.php
@@ -1,3 +1,7 @@
+<html>
+<head>
+<title>CU@MyPlace</title>
+<link rel="stylesheet" href="default.css">
 </head>
 
 <body>
@@ -37,6 +41,17 @@
       <tr><td>Tonny</td></tr>
       <tr><td>Kateiiz</td></tr>
       <tr><td>Tongkey</td></tr>
+      <tr><td>Wari</td></tr>
+      <tr><td></td></tr>
+
+      <tr><td></td></tr>     <!--เว้นช่องข้างล่าง-->
+    </table>
+
+    <table id="lastbar">
+      <!--แถบสุดท้าย-->
+      <tr>
+        <td>
+          <div class="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur imperdiet felis a eleifend varius. Quisque sodales sem eleifend purus dignissim convallis. Nulla ut est sit amet ligula sodales blandit.<br> Nullam nec erat pulvinar nulla iaculis tincidunt. Mauris bibendum congue ornare. Pellentesque quis massa tempus tortor facilisis vestibulum. Phasellus dictum eleifend tincidunt. Fusce congue dui et magna consequat,<br> at accumsan nisi consequat. Fusce ac nisl sem. Duis sed elit justo. Sed rhoncus fringilla lacinia. Integer posuere metus eu nibh feugiat,<br> semper efficitur diam luctus. Proin pellentesque dapibus tortor, ac condimentum tortor dictum suscipit.</div>
         </td>
       </tr>
       <tr>
