23224b61c8a721f00be8b7dc14265a81b973d1ab
katepp <ppkadt@gmail.com>
2017-11-05 14:46:32 +0700

    done

--- database/login.php
+++ database/login.php
@@ -7,17 +7,16 @@
 
 <body>
   <div id="menubar">
-    <!--แถบบน-->
     <div id="logo"><a href="home.php">CU@MyPlace</a></div>
     <ul id="menu">
-      <li><a href="room.php">Room</a></li>
-      <li><a href="facility.php">Facility</a></li>
-      <li><a href="review.php">Review</a></li>
-      <li><a href="login.php">Log In</a></li>
+      <li class="topbartext"><a href="room.php">Room</a></li>
+      <li class="topbartext"><a href="facility.php">Facility</a></li>
+      <li class="topbartext"><a href="review.php">Review</a></li>
+      <li class="topbartext"><a href="login.php">Log In</a></li>
     </ul>
   </div>
 
-  <table>
+  <table class="fullwidth" id="hometable">
     <div id="boxnewuser">
       <!--กล่อง register-->
       <form action="notice.php">
@@ -55,62 +54,34 @@
     </div>
 
     <tr>
-      <td><img src="img/view1.jpg" height="600" width="100%"></td>
+      <td colspan="2"><img src="img/view1.jpg" height="600px" width="100%"></td>
     </tr>
     <!--รูปวิวบนสุด-->
   </table>
 
-  <table id="bar">
-    <!--แถบรองสุดท้าย-->
-    <tr>
-      <td></td>
-    </tr>
-    <!--เว้นช่องข้างบน-->
-
-    <tr>
-      <td>
-        <h1>Database system</h1></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>Tonny</td>
-    </tr>
-    <tr>
-      <td>Kateiiz</td>
-    </tr>
-    <tr>
-      <td>Tongkey</td>
-    </tr>
-    <tr>
-      <td>Wari</td>
-    </tr>
-    <tr>
-      <td></td>
-    </tr>
-
-    <tr>
-      <td></td>
-    </tr>
-    <!--เว้นช่องข้างล่าง-->
-  </table>
+  <table id="bar"><!--แถบรองสุดท้าย-->
+    <tr><td><strong>CU@MyPlace : ITS322 Database System sec. 1</strong></td></tr>
+    <tr><td>&nbsp;- Kriddanai Roong-uthai 5822780334</td></tr>
+    <tr><td>&nbsp;- Prompat Tipphakdee 5822771317</td></tr>
+    <tr><td>&nbsp;- Apiwat Thaiphakdee 5822770467</td></tr>
+    <tr><td>&nbsp;- Wari Maroengsit 5822771333</td></tr>
+    </table>
 
   <table id="lastbar">
     <!--แถบสุดท้าย-->
     <tr>
       <td>
-        <div class="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur imperdiet felis a eleifend varius. Quisque sodales sem eleifend purus dignissim convallis. Nulla ut est sit amet ligula sodales blandit.<br> Nullam nec erat pulvinar nulla iaculis
-          tincidunt. Mauris bibendum congue ornare. Pellentesque quis massa tempus tortor facilisis vestibulum. Phasellus dictum eleifend tincidunt. Fusce congue dui et magna consequat,<br> at accumsan nisi consequat. Fusce ac nisl sem. Duis sed elit
-          justo. Sed rhoncus fringilla lacinia. Integer posuere metus eu nibh feugiat,<br> semper efficitur diam luctus. Proin pellentesque dapibus tortor, ac condimentum tortor dictum suscipit.</div>
+        <div class="centerlastbar">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur imperdiet felis a eleifend varius. Quisque sodales sem eleifend purus dignissim convallis. Nulla ut est sit amet ligula sodales blandit. Nullam nec erat pulvinar nulla iaculist incidunt. Mauris bibendum congue ornare. Pellentesque quis massa tempus vestibulum. Phasellus dictum eleifend tincidunt. Fusce congue dui et magna consequat, at accumsan nisi consequat. Fusce ac nisl sem. Duis sed elit justo. Sed rhoncus fringilla lacinia. </div>
       </td>
     </tr>
     <tr>
       <td>
-        <div class="center">Powered by M150</div>
+        <div class="centerlastbar">Powered by M150</div>
       </td>
     </tr>
 
   </table>
 
 </body>
+
+</html>
