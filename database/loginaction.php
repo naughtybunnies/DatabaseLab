ae0ca5660ab885a0c2ab9e8ad0dce70f91fa9868
tonny62 <ton_kriddanai@hotmail.com>
2017-11-06 21:19:05 +0700

    fix loginaction

--- database/loginaction.php
+++ database/loginaction.php
@@ -1,5 +1,4 @@
 <?php
-  session_start();
   require_once('connect.php');
   $q = "SELECT * FROM user WHERE user.email='".$_POST['email']."' AND user.password='".$_POST['password']."';";
   $result = $mysqli->query($q);
@@ -9,13 +8,14 @@
   }else{
     $row = $result->fetch_array();
     if($row){
+      session_start();
       #echo "login success";
       $_SESSION['user']=$row;
       if($row['usergroup_idusergroup']=='11'){
         echo "STAFF LOGIN";
       }else{
         #echo "CUSTOMER LOGIN";
-        if (isset($_SESSION['user'])) {
+        if (isset($_SESSION['myarray'])) {
           header('Location: addguest.php');
         }else{
           header('Location: customer.php');
