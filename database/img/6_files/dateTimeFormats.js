0ecf395b9b826507d9899507e4ddc4c62587d595
parewari <maroengsit.pwr@gmail.com>
2017-11-08 18:45:05 +0700

    facility

--- database/img/6_files/dateTimeFormats.js
+++
@@ -1,9 +0,0 @@
-; define('exp_datetimeformats', function() {
-
-	'use strict';
-
-window.uitkformatter = window.uitkformatter || {};
-window.uitkformatter.dateTimeSettings =  window.uitkformatter.dateTimeSettings || {};
-window.uitkformatter.dateTimeSettings.en_GB = {"calendarPlaceholder":"dd/mm/yyyy","time":{"format":{"full":"HH:mm:ss zzzz","medium":"HH:mm:ss","short":"H:mm","long":"H:mm z"}},"date":{"calendarPlaceholder":"dd/mm/yyyy","dateSeparators":"/","weekday":{"wide":{"wednesday":"Wednesday","thursday":"Thursday","monday":"Monday","tuesday":"Tuesday","saturday":"Saturday","friday":"Friday","sunday":"Sunday"},"abbreviated":{"wednesday":"Wed","thursday":"Thu","monday":"Mon","tuesday":"Tue","saturday":"Sat","friday":"Fri","sunday":"Sun"},"narrow":{"wednesday":"W","thursday":"T","monday":"M","tuesday":"T","saturday":"S","friday":"F","sunday":"S"}},"month":{"wide":{"february":"February","march":"March","april":"April","may":"May","june":"June","july":"July","august":"August","september":"September","october":"October","november":"November","december":"December","january":"January"},"abbreviated":{"february":"Feb","march":"Mar","april":"Apr","may":"May","june":"Jun","july":"Jul","august":"Aug","september":"Sep","october":"Oct","november":"Nov","december":"Dec","january":"Jan"},"narrow":{"february":"F","march":"M","april":"A","may":"M","june":"J","july":"J","august":"A","september":"S","october":"O","november":"N","december":"D","january":"J"}},"format":{"full":"EEE, d MMM","medium":"d MMM","short":"d/M/yyyy","long":"d MMM yyyy"}}};
-return window.uitkformatter;
-});
