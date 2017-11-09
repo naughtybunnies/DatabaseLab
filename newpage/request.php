4fa7d0e369ac833271cb9aa815d50b8a9a0ea084
katepp <ppkadt@gmail.com>
2017-11-09 19:20:23 +0700

    edit new request 2

--- newpage/request.php
+++ newpage/request.php
@@ -11,24 +11,20 @@
 
     <div class="container">
       <?php topbar_logout();?>
-      <div class="button_requestpage">
-        <a href="#">
-          <div id="sendrequest_button">
-            Send Request
-          </div>
-        </a>
 
-        <a href="#">
-          <div id="viewrequest_button">
-            View Request
-          </div>
-        </a>
+
+      <div id="request_button">
+        <ul>
+          <a href="#"><li>Send Request</li></a>
+          <a href="#"><li>Request</li></a>
+        </ul>
       </div>
 
       <td><img src="img/home1.jpg" height="600" width="100%"></td>
 
-      <?php footer(); ?>
-    </div>
+
+  <?php footer(); ?>
+</div>
 
   </body>
 
