9c5aacddb5cc965131dfbce2d20d9def31cd0264
katepp <ppkadt@gmail.com>
2017-11-09 16:28:44 +0700

    edit home.php 3

--- newpage/home.php
+++ newpage/home.php
@@ -11,6 +11,52 @@
     <?php topbar();?>
 
     <div id="seaview">
+      <div id="booking_box">
+
+        <form action="newbooking.php" method="POST">
+          <div class="textonbooking_box">
+            <br>
+            <b>Check availabity</b>
+
+              <p>Check in :<input type="date" name="datein"/>
+              <br><br>
+              Check out :<input type="date" name="dateout"/></p>
+
+
+
+            <b>Property</b>
+
+              <p>Room:
+              <select name="room">
+                    <?php
+                      for ($i=1; $i < 21; $i++) {
+                        echo '<option value="'.$i.'">'.$i.'</option>';
+                      }
+                    ?>
+                  </select> Adult:
+              <select name="adult">
+                    <?php
+                      for ($i=1; $i < 21; $i++) {
+                        echo '<option value="'.$i.'">'.$i.'</option>';
+                      }
+                    ?>
+                  </select> Children:
+              <select name="children">
+                    <option value="0">0</option>
+                    <?php
+                      for ($i=1; $i < 21; $i++) {
+                        echo '<option value="'.$i.'">'.$i.'</option>';
+                      }
+                    ?>
+                  </select>
+                </p>
+
+                <br><br>
+
+              <input type="submit" value="Check Availability" />
+          </div>
+        </form>
+      </div>
       <img src="img/home1.jpg" width="100%" height="600px">
       <div class="textonpic">
         <h1>
