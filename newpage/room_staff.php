4166da42081bed2eb711c2d621378497c02cd9eb
katepp <ppkadt@gmail.com>
2017-11-10 19:46:34 +0700

    staff's function

---
+++ newpage/room_staff.php
@@ -0,0 +1,33 @@
+<?php
+  require_once('helperfunction.php');
+  session_start();
+?>
+<html>
+<head>
+  <title>CU@MyPlace</title>
+  <link rel="stylesheet" href="default.css">
+</head>
+
+<body>
+
+    <div class="container">
+      <?php
+      if (isset($_SESSION['userinfo'])) {
+        //logged in
+        topbar_logout_staff();
+      }else{
+        topbar();
+      }
+       ?>
+
+
+
+      <td><img src="img/home1.jpg" height="600" width="100%"></td>
+
+
+  <?php footer(); ?>
+</div>
+
+  </body>
+
+</html>
