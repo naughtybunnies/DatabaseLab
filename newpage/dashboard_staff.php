acd06a6a22081441d97766bef326af8281a5f54b
parewari <maroengsit.pwr@gmail.com>
2017-11-10 18:16:47 +0700

    edit dashboard_staff
    insert small icon into button ja

--- newpage/dashboard_staff.php
+++ newpage/dashboard_staff.php
@@ -13,14 +13,14 @@
       <?php topbar_logout(); ?>
       <div id="dashboard_staff">
         <ul>
-          <a href="#"><li>Bookings</li></a>
-          <a href="#"><li>Staffs</li></a>
-          <a href="#"><li>Messages</li></a>
-          <a href="#"><li>Rooms</li></a>
-          <a href="#"><li>Requests</li></a>
-          <a href="#"><li>Users</li></a>
-          <a href="#"><li>Services</li></a>
-          <a href="#"><li>Transactions</li></a>
+          <a href="#"><li>Bookings <img src="img/i_booking1.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="#"><li>Staffs  <img src="img/i_staff.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="#"><li>Messages <img src="img/i_message.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="#"><li>Rooms <img src="img/i_room1.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="#"><li>Requests <img src="img/i_request.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="#"><li>Users <img src="img/user.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="#"><li>Services <img src="img/i_service.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="#"><li>Transactions <img src="img/i_transaction.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
           <a href="#"><li>Something</li></a>
           <a href="#"><li>Something</li></a>
           <a href="#"><li>Something</li></a>
