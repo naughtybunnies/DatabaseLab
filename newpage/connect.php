b04f860386e883623439616e0b87736d67f27e0d
kenjitong <oitip_thaiphakdee@hotmail.com>
2017-11-14 04:53:44 +0700

    commit

--- newpage/connect.php
+++ newpage/connect.php
@@ -1,5 +1,5 @@
 <?php
-  $mysqli = new mysqli('localhost', 'root', '', 'CU_FINAL');
+  $mysqli = new mysqli('localhost', 'root', 'root', 'CU_FINAL');
   if($mysqli->connect_errno){
     #echo "Connection Error";
   }else{
