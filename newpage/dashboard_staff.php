b30c2f53eb57e8ebb968ea4dc202ebe1c0a2c076
katepp <ppkadt@gmail.com>
2017-11-14 21:11:16 +0700

    rename in dashboard_staff.php

--- newpage/dashboard_staff.php
+++ newpage/dashboard_staff.php
@@ -14,12 +14,12 @@
       <div id="dashboard_staff">
         <ul>
           <a href="booking_staff.php"><li>Bookings <img src="img/i_booking1.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
-          <a href="staff.php"><li>Staffs  <img src="img/i_staff.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
-          <a href="message.php"><li>Messages <img src="img/i_message.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="staff_staff.php"><li>Staffs  <img src="img/i_staff.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="message_staff.php"><li>Messages <img src="img/i_message.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
           <a href="room_staff.php"><li>Rooms <img src="img/i_room1.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
           <a href="request_staff.php"><li>Requests <img src="img/i_request.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
-          <a href="user.php"><li>Users <img src="img/user.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
-          <a href="service.php"><li>Services <img src="img/i_service.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="user_staff.php"><li>Users <img src="img/user.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
+          <a href="service_staff.php"><li>Services <img src="img/i_service.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
           <a href="transaction_staff.php"><li>Transactions <img src="img/i_transaction.png" style="width:40px;height:40px;" alt="" align="center"></li></a>
           <a href="#"><li>Something</li></a>
           <a href="#"><li>Something</li></a>
