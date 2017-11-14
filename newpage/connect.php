58957208820699655dd718a6d25378e414eb1e42
parewari <maroengsit.pwr@gmail.com>
2017-11-14 22:36:57 +0700

    connect.php ofr wari
    มันบอกให้ commit ไม่งั้น pull ไม่ได้

--- newpage/connect.php
+++ newpage/connect.php
@@ -1,5 +1,8 @@
 <?php
-  $mysqli = new mysqli('localhost', 'root', '', 'CU_FINAL');
+
+
+  $mysqli = new mysqli('192.168.1.95', 'wari', 'wari', 'CU_FINAL');
+
   if($mysqli->connect_errno){
     #echo "Connection Error";
   }else{
