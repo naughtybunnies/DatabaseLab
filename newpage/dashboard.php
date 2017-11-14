a23dd74099b2b1117bba789d3a9e04ba1d5c5d2c
katepp <ppkadt@gmail.com>
2017-11-14 22:37:11 +0700

    new dashboard.php

--- newpage/dashboard.php
+++ newpage/dashboard.php
@@ -2,6 +2,12 @@
   require_once('helperfunction.php');
   session_start();
   if (isset($_SESSION['userinfo'])) {
+    if ($_SESSION['userinfo']['usergroup_idusergroup']==11) {
+      header('Location: dashboard_staff.php');
+    }else{
+      header('Location: dashboard.php');
+    }
+
     #print_r($_SESSION['userinfo']);
   }else{
     header('Location: login.php');
