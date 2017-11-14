282ccc2fb37344e163f0a0ae5092d0a5e96ed73c
katepp <ppkadt@gmail.com>
2017-11-14 17:42:06 +0700

    booking_staff.php

--- newpage/booking_staff.php
+++ newpage/booking_staff.php
@@ -19,7 +19,14 @@
         topbar();
       }
        ?>
-
+       <div id="booking_staff_button">
+         <p>Booking</p>
+         <ul>
+           <a href="#"><li>Create <img src="img/i_request.png" style="width:40px;height:40px;" align="center"></li></a>
+           <a href="#"><li>Edit <img src="img/i_view.png" style="width:40px;height:40px;" align="center"></li></a>
+           <a href="#"><li>View <img src="img/i_view.png" style="width:40px;height:40px;" align="center"></li></a>
+         </ul>
+       </div>
       <td><img src="img/home1.jpg" height="600" width="100%"></td>
 
 
