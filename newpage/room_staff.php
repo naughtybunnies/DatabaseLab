c1b0e1f77694b788e1493861beaa6f167c79d6de
katepp <ppkadt@gmail.com>
2017-11-14 18:43:03 +0700

    service & room_staff & user

--- newpage/room_staff.php
+++ newpage/room_staff.php
@@ -19,9 +19,14 @@
         topbar();
       }
        ?>
-
-
-
+       <div id="room_staff_button">
+         <p>Room</p>
+         <ul>
+           <a href="#"><li>Create <img src="img/i_request.png" style="width:40px;height:40px;" align="center"></li></a>
+           <a href="#"><li>Edit <img src="img/i_view.png" style="width:40px;height:40px;" align="center"></li></a>
+           <a href="#"><li>View <img src="img/i_view.png" style="width:40px;height:40px;" align="center"></li></a>
+         </ul>
+       </div>
       <td><img src="img/home1.jpg" height="600" width="100%"></td>
 
 
